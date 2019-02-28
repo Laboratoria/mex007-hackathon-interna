3f28653874adcba7d86ca40149cc00b7ad0ba924
elenore29 <elenore.nava@gmail.com>
2019-02-27 22:05:56 -0600

    Cambiando los id por titulos de peliculas

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 const cardMovie = document.getElementById('card-movie');
 
-//const newData = JSON.parse(localStorage.getItem('movies'))
-let idArray = ['tt3896198', 'tt0076759', 'tt1371111'];
+//Star Wars: Episode IV, Cloud Atlas, Guardians of the Galaxy Vol. 2/ Avatar
+
+
+
+let idArray = ['Harry Potter and the Deathly Hallows: Part 2', 'Guardians of the Galaxy Vol. 2', 'Back to the Future', 'Avatar', 'Mulan', 'WALL·E', 'Toy Story', 'Back to the Future', 'Batman', 'Pacific Rim: Uprising', 'Die Hard', 'Frozen'];
 let allMovies = [];
 let movieData =[];
 let apiKey = '96fb7a85';
 const moviesData = () => {
     for (let i = 0; i < idArray.length; i++) {
         //Fetch para traer la data del archivo lol.json
-        fetch ('https://www.omdbapi.com/?i='+idArray[i]+'&apikey='+apiKey)
+        fetch ('https://www.omdbapi.com/?t='+idArray[i]+'&apikey='+apiKey)
         .then (response => response.json())
         .then (data=>{
         movieData = data;
+        console.log(data)
         allMovies.push(movieData);
         return allMovies
         })
