9102b0d1c3ff4239c89ac3bed302da772d3efef4
jonh <jonathan@laboratoria.la>
2019-02-28 11:57:13 -0600

    probando el redireccionamiento en pages. regresando la linea 1, la comparacin para el path login desde pages poniendo el html al final

--- src/js/firebase/auth.js
+++ src/js/firebase/auth.js
@@ -1,4 +1,4 @@
-if (window.location.pathname === '/src/views/login' || window.location.pathname === '/privado/views/login') {
+if (window.location.pathname === '/src/views/login' || window.location.pathname === '/privado/src/views/login.html') {
 console.log(location.pathname)
   const signIn = document.getElementById('sign-in')
 
