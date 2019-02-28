dd6e7d45610a23f17d5a32d87c132eb7bbdcfb2d
Champy13 <hgftdrd@hotmail.com>
2019-02-28 16:57:14 -0600

    Se crea función para ornar por ranking y generar el array de  top 10

--- src/data.js
+++ src/data.js
@@ -1,28 +1,17 @@
 window.data = {
 
-    filterByConside: (dataMovieList, letterValue) => {
-        
-      const searConside = dataMovieList.filter(dataMovieList => (dataMovieList.Title.toLowerCase().match(letterValue.toLowerCase()) 
-                                                                  ||dataMovieList.Director.toLowerCase().match(letterValue.toLowerCase())
-                                                                  ||dataMovieList.Year.match(letterValue)));
-        return searConside;
-      
+filterByConside: (dataMovieList, letterValue) => {
+
+    const searConside = dataMovieList.filter(dataMovieList => (dataMovieList.Title.toLowerCase().match(letterValue.toLowerCase()) ||
+            dataMovieList.Director.toLowerCase().match(letterValue.toLowerCase()) ||
+            dataMovieList.Year.match(letterValue)));
+    return searConside;
+},
+
+
+
+orderData: (dataMovie) => {
+
+    return dataMovie.sort((a, b) => (parseFloat(b.imdbRating) - parseFloat(a.imdbRating)));;
     }
-
-
-    
-// orderData: (dataMovie, sortBy, sortOrder) => {
-// dataMovie.map (element => {
-//     let element;
-// return element.Title;
-
-// });
-
-// if (sortOrder == 'asc') {
-//     return data.sort((a, b) => (a[sortBy] > b[sortBy]) ? 1 : -1 );
-// }
-
-// }
-    
-
 };
