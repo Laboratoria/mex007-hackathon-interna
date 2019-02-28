3efefc4c9697d426d74eceec34c2eaefae063a62
Diana <aries_di@hotmail.com>
2019-02-27 21:09:14 -0600

    Actualizando rama

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
-fetch('http://www.omdbapi.com/?t=titanic')
-.then ((data) => data.json())
-.then((Title) => {return console.log(Title)})   
+let dataCinema="";
+fetch('http://www.omdbapi.com/?i=tt3896198&apikey=91de21ce')
+.then (data => data.json())
+.then (data => {
+    dataCinema = data;
+    localStorage.setItem("data", JSON.stringify(dataCinema))
+})
+
+dataCinema = JSON.parse(localStorage.getItem("data"));
+const prueba = (dataCinema) => {
+    dataCinema.forEach(element => {
+        let title = element.Title;
+        let year = element.Year;
+        return(title,year)
+    });
+}
+
+prueba.innerHTML = `<p> Titulo: ${dataCinema.Title}</p>
+<p> Año: ${dataCinema.Year}</p>`;
+
+alert('Hola')
