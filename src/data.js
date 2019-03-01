b3dc3d1d89857a9d0deef05033c927a8ead0a3e0
ameyallibg <bgameyalli@gmail.com>
2019-03-01 13:01:17 -0600

    funcion de filtrar

--- src/data.js
+++ src/data.js
@@ -1,8 +1,14 @@
 window.data = {
 filterInput: (dataMovies, letter) => {
 
-    const search = dataMovies.filter(dataMovies => (dataMovies.Title.toLowerCase().match(letter.toLowerCase()) || dataMovies.Director.toLowerCase().match(letter.toLowerCase())));
+    const search = dataMovies.filter(dataMovies =>(dataMovies.Title.toLowerCase().match(letter.toLowerCase()) || dataMovies.Director.toLowerCase().match(letter.toLowerCase())));
+    
+    if(search == ""){
+    alert("Sorry, busqueda sin éxtio por las siguientes razones: \n -No se clasifica como cine de culto \n -No se encuentra aun con nuestra base de datos. Seguiremos trabajando")
+    }
+    else{
     return search;
+    }
 
   },
 
@@ -10,4 +16,15 @@ filterInput: (dataMovies, letter) => {
     const arrayFilteredPokemons = dataMovies.filter(dataMovies => dataMovies.Genre.includes(optionFilter))
     return arrayFilteredPokemons
   },
+  
+  filterByAwards: (dataMovies) => {
+    const arrayFilteredPokemons = dataMovies.filter(dataMovies => dataMovies.Awards.includes("wins"))
+      return arrayFilteredPokemons
+    },
+
+    sortData: (dataMovies) => {
+      const sortByName = dataMovies.sort((sortOrderA, sortOrderZ) => parseFloat(sortOrderA.imdbRating - sortOrderZ.imdbRating) ? -1 : 1);
+      return sortByName;
+    },
+    
 }
