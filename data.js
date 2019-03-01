724e489c4c57c3b8cccc62ebbe2b7333dec0a01a
JazminDominguez <j.dgez.p@gmail.com>
2019-02-28 18:07:54 -0600

    implementación de filtro por generos, datos de año y género en tarjetas de películas

--- data.js
+++ data.js
@@ -3,6 +3,12 @@ filterInput: (baseDatos, letter) => {
 
     const search = baseDatos.filter(baseDatos => (baseDatos.Title.toLowerCase().match(letter.toLowerCase()) || baseDatos.Director.toLowerCase().match(letter.toLowerCase())));
     return search;
-
   },
+
+
+filterByGenre: (genreId, baseDatos) => {
+  const filterGenreInData = baseDatos.filter(baseDatos => baseDatos.Genre.includes(genreId));
+   return filterGenreInData;
+
+},
 }
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
- const artMovies = ["vertigo","Citizen Kane", "Tokyo Story","2001: A Space Odyssey", "Man With A Camera","In the Mood for Love","Rashomon"," Andrei Rublev","Mulholland Dr.","Stalker","Shoah","El Padrino II","Taxi Driver","Metropolis","Psycho","Jeanne Dielman, 23 Commerce Quay, 1080 Brussels", "Satantango", "La Dolce Vita", " Pather Panchali" ]
+//Eventos del DOM
+const genreClass = document.getElementsByClassName("genre");
+const genreButton = document.getElementById("genreButton")
+
+
+
+const artMovies = ["vertigo","Citizen Kane", "Tokyo Story","2001: A Space Odyssey", "Man With A Camera","In the Mood for Love","Rashomon"," Andrei Rublev","Mulholland Dr.","Stalker","Shoah","El Padrino II","Taxi Driver","Metropolis","Psycho","Jeanne Dielman, 23 Commerce Quay, 1080 Brussels", "Satantango", "La Dolce Vita", " Pather Panchali" ]
 const returnFilter = document.getElementById('return-filter');
 const browser = document.getElementById("browser");
 let baseDatos = []
@@ -21,6 +27,9 @@ let baseDatos = []
  })
 }
 
+
+let usableGenreData =[];
+
 const drawMovies = (arrayPokemons) => {
     const sectionRoot = returnFilter
     sectionRoot.innerHTML = "";
@@ -28,11 +37,14 @@ const drawMovies = (arrayPokemons) => {
       let movieData = `
       <div class="card">
         <p class="title">${baseDatos.Title}</p>
+        <p class="year">${baseDatos.Year}</p>
         <p class="plot">°${baseDatos.Plot}</p>
+        <p class="genre">°${baseDatos.Genre}</p>
         <img class="poster" src="${baseDatos.Poster}"/>
         <p>${baseDatos.Director}</p>
       </div>`;
       sectionRoot.insertAdjacentHTML("beforeend", movieData)
+      usableGenreData
     })
 
 }
@@ -46,11 +58,42 @@ const filterLetter = () => {
   filterLetter();
 
 
-    
+//filtros por género de película
+const selectByGenre = () => {
+
+  for(let i=0; i < genreClass.length; i++){
+    genreClass[i].addEventListener('click',() =>{
+    let genreId = genreClass[i].id;
+    console.log(genreId)
+     returnFilter.innerHTML = "";
+     
+    const arraySelectedGenre = window.data.filterByGenre(genreId,baseDatos);
+    drawMovies(arraySelectedGenre);
+      
+  })}
+}
+selectByGenre();
 
 
 
 
+
+/*const selectByRol = () => {
+  for (let i = 0; i < rol.length; i++) {
+    rol[i].addEventListener('click', () => {
+      let rolId = rol[i].id;
+      document.getElementById("mySidenav").style.width = "0%";
+      printRolTitle.innerHTML = "";
+      printRolTitle.insertAdjacentHTML("beforeend", rolId);
+
+      const arrayRolesFiltered = window.lol.filtroDataRoles(rolId, dataLol);
+      document.getElementById("welcomeMssg").style.display = 'none';
+      printRolTitle.style.display = "block";
+      print(arrayRolesFiltered);
+    })
+  }
+}
+selectByRol();*/
 
 
 
